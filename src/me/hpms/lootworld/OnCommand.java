75e51d3e4a09151e00aa107dbf22cef36c039e6e
hpmss <arestvn@gmail.com>
2019-01-16 16:39:03 +0700

    #; git commit -m #

--- src/me/hpms/lootworld/OnCommand.java
+++ src/me/hpms/lootworld/OnCommand.java
@@ -4,13 +4,9 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
-import net.minecraft.server.v1_12_R1.Packet;
-import net.minecraft.server.v1_12_R1.PacketPlayOutBed;
 
 public class OnCommand implements CommandExecutor{
 	
@@ -28,10 +24,9 @@ public class OnCommand implements CommandExecutor{
 		item.setItemMeta(meta);
 		p.getInventory().addItem(item);
 		
-		Packet packet = new PacketPlayOutBed();
-		CraftPlayer pc = (CraftPlayer) p;
-		pc.getHandle().playerConnection.sendPacket(packet);
-		
+		@SuppressWarnings("unused")
+		Lay lay = new Lay(p);
+	
 		return false;
 	}
 
